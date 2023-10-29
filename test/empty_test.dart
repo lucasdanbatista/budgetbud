87d17919f7f8db9ff1d293489c24f3413e690577
Lucas Batista <contact@lucasbatista.me>
2023-10-29 14:29:40 -0300

    Add Flutter pipeline (#1)

---
+++ test/empty_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+/// A empty test that keep CI/CD tools passing when there aren't anothers
+/// tests to run.
+void main() => test('', () => expect('', ''));
