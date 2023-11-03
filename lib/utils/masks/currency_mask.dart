22dcfba4860befc0da47738f80c6bfb231c73f59
Lucas Batista <contact@lucasbatista.me>
2023-11-03 12:19:47 -0300

    Fix currency mask (#13)

--- lib/utils/masks/currency_mask.dart
+++ lib/utils/masks/currency_mask.dart
@@ -3,12 +3,21 @@ import 'package:easy_mask/easy_mask.dart';
 class CurrencyMask extends TextInputMask {
   CurrencyMask()
       : super(
-          mask: 'R!\$! !9+.999,99',
+          mask: '9+.999,99',
           placeholder: '0',
+          maxPlaceHolders: 3,
           reverse: true,
         );
 
-  double unmask(String text) => double.parse(
-        text.replaceAll('R\$', '').replaceAll('.', '').replaceAll(',', '.'),
+  String maskValue(double value) {
+    final regex = RegExp(r'\d(?=(\d{3})+,)');
+    return value
+        .toStringAsFixed(2)
+        .replaceAll('.', ',')
+        .replaceAllMapped(regex, (m) => '${m[0]}.');
+  }
+
+  double unmaskText(String text) => double.parse(
+        text.replaceAll('.0', '00').replaceAll('.', '').replaceAll(',', '.'),
       );
 }
